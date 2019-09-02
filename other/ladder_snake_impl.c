6bbdafad1867d40621549f4e73396b716dcd84aa
onval <giorgio.valbonesi@gmail.com>
2019-09-02 16:30:15 +0200

    Fix ladder snake code to use dinamic programming for sequence creation

--- other/ladder_snake_impl.c
+++ other/ladder_snake_impl.c
@@ -10,11 +10,11 @@ typedef struct node_item_t {
     int throws;
 } node_item_t;
 
-
 node_item_t *createItem(int value, int throws);
 int nodeValue(int *board, int length, int node_index);
 int minThrows(int *board, int length, int starting_node, bool print_seq, bool ignore_ls);
 void print_item(void *item);
+void print_sequence(void *s);
 
 int main() {
     char command, option;
@@ -64,14 +64,17 @@ int main() {
     return 0;
 }
 
-//this version of the algorithm enters an infinite loop 
-//if board is not built correctly or a path doesn't exists
 int minThrows(int *board, int length, int starting_node, bool print_seq, bool ignore_ls) {
-    int next_node;
-    int best_throw;
     int *visited = calloc(length, sizeof(int));
 
     linked_list *queue = NULL;
+    int throws;
+
+    int **sequences;
+    sequences = malloc(31 * sizeof(int *));
+    for (int i=0; i < 31; i++) {
+        sequences[i] = calloc(10, sizeof(int));
+    }
 
     node_item_t *item = createItem(starting_node, 0);
     enqueue (&queue, item);
@@ -80,6 +83,7 @@ int minThrows(int *board, int length, int starting_node, bool print_seq, bool ig
     node_item_t *temp_item;
 
     visited[item->value] = 1;
+    int next_node;
 
     //breadth first search, using a queue
     while(queue != NULL) {
@@ -88,21 +92,36 @@ int minThrows(int *board, int length, int starting_node, bool print_seq, bool ig
             continue;
         } else {
             if (current_item->value == length) {
+                if (print_seq) {
+                    for (int i=0; sequences[length][i] != 0; i++) {
+                        printf("%d ", sequences[length][i]);
+                    }
+                }
                 return current_item->throws;
             }
         }
 
-        for (int i=0; i < MAX_DICE_OUTPUT; i++) {
-            next_node = nodeValue(board, length, current_item->value + i+1);
-            //SEEMS LIKE MY QUEUE LIB DOESN'T WORK TOO WELL AFTER ALL
-            if (next_node <= length && visited[next_node] == 0) {
-                temp_item = createItem(next_node, current_item->throws+1);
+        for (int i=1; i <= MAX_DICE_OUTPUT; i++) {
+            
+            next_node = (ignore_ls) ? current_item->value+i : 
+                            nodeValue(board, length, current_item->value + i);
+
+            if (next_node >=0 && next_node <= length && visited[next_node] == 0) {
+                throws = current_item->throws;
+                temp_item = createItem(next_node, throws+1);
                 enqueue(&queue, temp_item);
+                sequences[next_node][throws] = i;
+
+                while (throws >= 1) {
+                    sequences[next_node][throws-1] = 
+                        sequences[current_item->value][throws-1];
+                    throws--;
+                }
+                
                 visited[next_node] = 1;
             }
         }
-
-        print_ll(queue, print_item);
+        // print_ll(queue, print_item);
     }
 
     return -1;
@@ -129,4 +148,8 @@ node_item_t *createItem(int value, int throws) {
 void print_item(void *item) {
     printf("v=%d (t=%d)", ((node_item_t *)item)->value, 
                                     ((node_item_t *) item)->throws);
+}
+
+void print_sequence(void *s) {
+    printf("-> %d ", *((int *)s));
 }
